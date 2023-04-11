935ad9c564e6ff104997623e7a5c79926754c4a2
footballjones <footballjones308@gmail.com>
2023-04-11 14:59:52 -0400

    Completed

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(drivers, names) {
+    return drivers.filter(drivers => drivers.toLowerCase() === names.toLowerCase())
+}
+
+function fuzzyMatch(drivers, names) {
+    return drivers.filter(drivers => drivers[0] === names[0])
+}
+
+function matchName(drivers, names) {
+    return drivers.filter(driver => driver.name === names)
+}
